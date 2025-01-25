bd9f6ff6e01a7591987ffb00898a39bc0486011e
Pascal Töpke <pascal.toepke@gmail.com>
2025-01-25 02:28:03 +0100

    Update ingress_nginx Helm release configuration to include version and refactor set blocks

--- ingress_nginx/ingress_nginx.tf
+++ ingress_nginx/ingress_nginx.tf
@@ -1,18 +1,18 @@
 resource "helm_release" "ingress_nginx" {
-  name       = "ingress_nginx"
-  chart      = "ingress-nginx"
-  repository = "https://kubernetes.github.io/ingress-nginx"
-  namespace  = "ingress-nginx"
-
+  name             = "ingress_nginx"
+  chart            = "ingress-nginx"
+  repository       = "https://kubernetes.github.io/ingress-nginx"
+  namespace        = "ingress-nginx"
+  version          = "4.12.0"
   create_namespace = true
 
-  set {
+  set = [{
     name  = "controller.kind"
     value = "DaemonSet"
-  }
+    },
+    {
+      name  = "controller.ingressClassResource.default"
+      value = "true"
+  }]
 
-  set {
-    name  = "controller.ingressClassResource.default"
-    value = "true"
-  }
 }
