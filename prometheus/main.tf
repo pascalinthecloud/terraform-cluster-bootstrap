1d73a81b8c6345d18bf0b6c8f3ad6570ed05c0a7
Pascal Töpke <pascal.toepke@gmail.com>
2025-01-26 21:48:32 +0100

    feat: update prometheus namespace to 'monitoring' and set service type to LoadBalancer

--- prometheus/main.tf
+++ prometheus/main.tf
@@ -2,7 +2,14 @@ resource "helm_release" "prometheus" {
   name             = "prometheus"
   chart            = "kube-prometheus-stack"
   repository       = "https://prometheus-community.github.io/helm-charts"
-  namespace        = "prometheus"
+  namespace        = "monitoring"
   version          = "68.3.0"
   create_namespace = true
+
+  set = [{
+    name  = "prometheus.service.type"
+    value = "LoadBalancer"
+    }
+    ]
+
 }
