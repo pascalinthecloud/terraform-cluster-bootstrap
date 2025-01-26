53ade6e110e506eb1787850be091796fa0889416
Pascal Töpke <pascal.toepke@gmail.com>
2025-01-26 22:42:40 +0100

    feat: add kubectl manifest for monitoring namespace and update helm release dependencies

--- prometheus/main.tf
+++ prometheus/main.tf
@@ -1,11 +1,20 @@
+resource "kubectl_manifest" "namespace" {
+    yaml_body = <<YAML
+apiVersion: v1
+kind: Namespace
+metadata:
+  name: monitoring
+  labels:
+    pod-security.kubernetes.io/enforce: privileged
+YAML
+}
 resource "helm_release" "prometheus" {
+  depends_on = [ kubectl_manifest.namespace ]
   name             = "prometheus"
   chart            = "kube-prometheus-stack"
   repository       = "https://prometheus-community.github.io/helm-charts"
   namespace        = "monitoring"
   version          = "68.3.0"
-  create_namespace = true
-
   set = [{
     name  = "prometheus.service.type"
     value = "LoadBalancer"
