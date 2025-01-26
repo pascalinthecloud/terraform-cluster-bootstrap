49bb323071b06474638f88979b5cdd2115bb94ee
Pascal Töpke <pascal.toepke@gmail.com>
2025-01-26 20:50:13 +0100

    fix: error

--- metallb/main.tf
+++ metallb/main.tf
@@ -1,30 +1,30 @@
-resource "kubernetes_namespace" "metallb_system" {
-  metadata {
-    name = "metallb-system"
-    labels = {
-      "pod-security.kubernetes.io/enforce" = "privileged"
-      "pod-security.kubernetes.io/audit"   = "privileged"
-      "pod-security.kubernetes.io/warn"    = "privileged"
-    }
-  }
+resource "kubectl_manifest" "namespace" {
+    yaml_body = <<YAML
+apiVersion: v1
+kind: Namespace
+metadata:
+  name: metallb-system
+  labels:
+    pod-security.kubernetes.io/enforce: privileged
+    pod-security.kubernetes.io/audit: privileged
+    pod-security.kubernetes.io/warn: privileged
+YAML
 }
 resource "helm_release" "metallb" {
-  depends_on = [kubernetes_namespace.metallb_system]
+  depends_on = [kubectl_manifest.namespace]
   name       = "metallb"
   chart      = "metallb"
   repository = "https://metallb.github.io/metallb"
   namespace  = "metallb-system"
 }
-
-resource "kubernetes_manifest" "ip_pool" {
+resource "kubectl_manifest" "ip_pool" {
   depends_on = [helm_release.metallb]
-  provider   = kubernetes.default
-  manifest   = file("${path.module}/configs/ip_pool.yaml")
+  yaml_body   = templatefile("${path.module}/configs/ip_pool.yaml", 
+  {
+    "ip_range" = var.ip_range
+  })
 }
-
-resource "kubernetes_manifest" "l2_advertisement" {
+resource "kubectl_manifest" "l2_advertisement" {
   depends_on = [helm_release.metallb]
-  provider   = kubernetes.default
-  manifest   = file("${path.module}/configs/l2_advertisement.yaml")
+  yaml_body   = file("${path.module}/configs/l2_advertisement.yaml")
 }
-
